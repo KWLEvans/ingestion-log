94525b89ff0c1efe4d3845a0cbc6b351a3b1c010
Keith Evans <kwlevans@gmail.com>
2017-03-31 09:03:16 -0700

    build logList data on app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Log } from './models/log.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  logList: Log[] = [
+    {
+      'name': 'Burger: Ham',
+      'calories': 465,
+      'details': 'Subject seemed excited about Burger despite very apparent lack of nutritional value. After completing ingestion it requested another multiple times before becoming comatose.'
+    }, {
+      'name': 'Sand Witch',
+      'calories': 20000,
+      'details': 'After repeatedly demanding a Sand Witch, subject seemed put off at the prospect of eating another of its species. After several days, it acquiesced and devoured the Witch of the Sands in its entirety. Subject has been lethargic and in a near catatonic state since.'
+    }, {
+      'name': 'Toast',
+      'calories': 50,
+      'details': 'Subject responded positively to toasted bread, but began to lament upon noticing that it did not contain \"butter\".'
+    }, {
+      'name': 'Celery',
+      'calories': -25,
+      'details': 'I regret the choice of celery. It has negatively impacted overall specimen growth.'
+    }
+  ];
 }
--- src/app/models/log.model.ts
+++ src/app/models/log.model.ts
@@ -1,3 +1,3 @@
-class Log {
+export class Log {
   constructor(public name: string, public calories: number, public details: string) {}
 }
